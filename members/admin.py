c132851c61077614c9e86cc152011b2f485960db
Pragya Agrawal <pragya.agrwal@gmail.com>
2023-07-31 21:15:51 +0530

    Added age attribute in member model

--- members/admin.py
+++ members/admin.py
@@ -4,6 +4,14 @@ import csv
 from django.contrib.admin.helpers import ActionForm, AdminForm
 from django import forms
 
+from datetime import datetime, date
+ 
+def calculate_age(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year - ((today.month, today.day) <
+         (birthDate.month, birthDate.day))
+    return age
+
 class DataImport(ActionForm):
     
     file = forms.FileField(required=False)
@@ -23,13 +31,18 @@ class MemberAdmin(admin.ModelAdmin):
                 input_data.member_id = row[0]
                 input_data.member_name = row[1]
                 input_data.gender = Member.Gender(row[2])
+                if row[3] != '':
+                    dob = datetime.strptime(row[3], "%Y-%m-%d").date()
+                    input_data.age = calculate_age(dob)
+                else:
+                    input_data.age = 0
                 input_data.save()
 
     actions = (
         'import_members',
     )
     change_list_template = "slots/admin_form.html"
-    list_display = ("member_id", "member_name", "gender",)
+    list_display = ("member_id", "member_name", "gender", "age", )
     search_fields = (
         'member_name',
         'member_id',
--- members/models.py
+++ members/models.py
@@ -14,5 +14,7 @@ class Member(models.Model):
 
     gender = models.CharField(max_length=30, choices=Gender.choices, default=Gender.female, null=True)
 
+    age = models.IntegerField(null=True)
+
     def __str__(self) -> str:
-        return str(self.member_id) + " - " + str(self.member_name)
+        return str(self.member_id) + " - " + str(self.member_name) + " - " + str(self.age)
