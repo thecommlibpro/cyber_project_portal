be7a662852159a132a34e511aa68926578e5a5f1
Pragya Agrawal <pragya.agrwal@gmail.com>
2023-10-07 13:10:07 +0530

    Couldn't load css in prod

--- portal/urls.py
+++ portal/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("slots.urls")),
     # path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
