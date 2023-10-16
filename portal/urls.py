3affff674f79de0c15f4d35781bdb587e6cd5fa9
Pragya Agrawal <pragya.agrwal@gmail.com>
2023-10-16 10:08:47 +0530

    Added logo media feature

--- portal/urls.py
+++ portal/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import re
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+
+#print(settings.MEDIA_URL, settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("slots.urls")),
-    # path("__debug__/", include("debug_toolbar.urls")),
-] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("slots/", include("slots.urls")),
+    re_path(
+        r"^%s(?P<path>.*)$" % re.escape(settings.MEDIA_URL.lstrip("/")), serve, kwargs={'document_root':settings.MEDIA_ROOT},
+    ),
+]
